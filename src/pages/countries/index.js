de5c0b7dd9bf7cb9df0a4da7263ac204dfdb1d34
mmaruf23 <maruuf82@gmail.com>
2025-02-03 17:17:43 +0700

    ssr pada halaman countries

--- src/pages/countries/index.js
+++ src/pages/countries/index.js
@@ -1,10 +1,11 @@
 import Card from '@/components/molecules/CountryCard';
 import Layout from '@/components/templates/Layout';
 import useCountries from '@/hooks/useCountries';
+import { getAllCountries } from '@/services/getCountries';
 import { useCallback, useEffect, useState } from 'react';
 
-export default function Countries() {
-  const countries = useCountries();
+export default function Countries({ countries }) {
+  // const countries = useCountries();
 
   const [search, setSearch] = useState([]);
   useEffect(() => {
@@ -13,17 +14,20 @@ export default function Countries() {
     }
   }, [countries]);
 
-  
-
-  const handleChange = useCallback((e) => {
-    const text = e.target.value;
-    if (text.length >= 1){
-      const filtered = countries.filter(item => item.name.common.toLowerCase().includes(text.toLowerCase()))
-      setSearch(filtered)
-    } else {
-      setSearch(countries)
-    }
-  }, [countries])
+  const handleChange = useCallback(
+    (e) => {
+      const text = e.target.value;
+      if (text.length >= 1) {
+        const filtered = countries.filter((item) =>
+          item.name.common.toLowerCase().includes(text.toLowerCase())
+        );
+        setSearch(filtered);
+      } else {
+        setSearch(countries);
+      }
+    },
+    [countries]
+  );
 
   return (
     <Layout>
@@ -39,7 +43,11 @@ export default function Countries() {
         <div className="lg:mt-10 mt-5 p-5">
           <div className="flex gap-5 flex-wrap justify-center">
             {search?.map((country) => (
-              <Card key={country.name.common} name={country.name} cca3={country.cca3}/>
+              <Card
+                key={country.name.common}
+                name={country.name}
+                cca3={country.cca3}
+              />
             ))}
           </div>
         </div>
@@ -47,3 +55,11 @@ export default function Countries() {
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const countries = await getAllCountries();
+
+  return {
+    props: { countries },
+  };
+};
